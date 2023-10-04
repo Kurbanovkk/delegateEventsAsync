0c89fc7cb47f98c63b1e270757fd3d3991a0e6e0
Kurban <kurbanovkk96@mail.ru>
2023-10-04 20:35:16 +0300

    Добавьте файлы проекта.

---
+++ delegateEventsAsync/ImageDownloader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegateEventsAsync
+{
+    internal class ImageDownloader
+    {
+        public event EventHandler<string>? ImageStarted;
+        public event EventHandler<string>? ImageCompleted;
+        public async Task<bool> DownloadAsync(string remoteUri, string fileName, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var myWebClient = new WebClient();
+                ImageStarted?.Invoke(this, fileName);
+                var downloadTask = myWebClient.DownloadFileTaskAsync(new Uri(remoteUri), fileName);
+                await Task.WhenAny(downloadTask, Task.Delay(Timeout.Infinite, cancellationToken));
+                if (cancellationToken.IsCancellationRequested)
+                    return false;
+                await Task.Delay(1000);
+                ImageCompleted?.Invoke(this, fileName);
+
+                return downloadTask.IsCompleted;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка при загрузке изображения: " + ex.Message);
+                return false;
+            }
+        }
+
+    }
+}
---
+++ delegateEventsAsync/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace delegateEventsAsync
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            List<string> imageUrls = new List<string>
+        {
+            "https://example.com/image1.jpg",
+            "https://example.com/image2.jpg",
+            "https://example.com/image3.jpg",
+            "https://example.com/image4.jpg",
+            "https://example.com/image5.jpg",
+            "https://example.com/image6.jpg",
+            "https://example.com/image7.jpg",
+            "https://example.com/image8.jpg",
+            "https://example.com/image9.jpg",
+            "https://example.com/image10.jpg",
+        };
+            var cts = new CancellationTokenSource();
+            CancellationToken cancellationToken = cts.Token;
+            var downloader = new ImageDownloader();
+            downloader.ImageStarted += (sender, fileName) =>
+            {
+                Console.WriteLine($"Скачивание файла {fileName} началось.");
+            };
+
+            downloader.ImageCompleted += (sender, fileName) =>
+            {
+                Task.Delay(1000);
+                Console.WriteLine($"Скачивание файла {fileName} завершено.");
+            };
+
+            var downloadTasks = new List<Task>();
+
+            foreach (var url in imageUrls)
+            {
+                var fileName = Path.GetFileName(url);
+                downloadTasks.Add(downloader.DownloadAsync(url, fileName, cancellationToken));
+            }
+            Console.WriteLine("Для отмены нажмите клавишу 'A'...");
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    if (Console.ReadKey(intercept: true).Key == ConsoleKey.A)
+                    {
+                        Console.WriteLine("Отмена всех загрузок...");
+                        foreach (var task in downloadTasks)
+                        {
+                           cts.Cancel();
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nСостояние загрузки:");
+                        for (int i = 0; i < imageUrls.Count; i++)
+                        {
+                            Console.WriteLine($"{imageUrls[i]} - {downloadTasks[i].IsCompleted}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
